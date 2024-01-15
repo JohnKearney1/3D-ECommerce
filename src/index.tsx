d1abfa5102e292229e527e2fd4f8d3256f3d95b9
John Kearney <John@kearneyjohn.com>
2024-01-15 05:41:22 -0500

    Vercel Analytics and Speed Insights

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -12,6 +13,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Analytics />
+    <SpeedInsights />
     <Router>
       <Routes>
         <Route path='/' element={<App />} />
